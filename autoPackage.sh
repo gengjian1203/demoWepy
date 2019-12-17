cc20b4ead98d2a74fcbf7f3121dd6a6eaac19a0b
gengjian1203 <gengjian1203@foxmail.com>
2019-12-17 20:36:32 +0800

    兼容Windows

--- autoPackage.sh
+++ autoPackage.sh
@@ -1,30 +1,42 @@
 #!/bin/bash
 ##################################################
-# setJsonBoolean
+# getJsonString
+# 获取json 字符串型字段
+# $1 文件
+# $2 键
+##################################################
+getJsonString() {
+  echo "getJsonString($*)"
+  #grep -Po 'extAppid[": ]+\K[^"]+' ${1}
+}
+
+##################################################
+# setJsonBooleanWin
 # 修改json 布尔型字段
 # $1 文件
 # $2 键
 # $3 值
 ##################################################
-setJsonBoolean() {
-  echo "setJsonBoolean($*)"
-  sed -i '' 's/\("'"${2}"'": \).*/\1'"${3}"',/g' ${1}
+setJsonBooleanWin() {
+  echo "setJsonBooleanWin($*)"
+  sed -i 's/\("'"${2}"'": \).*/\1'"${3}"',/g' ${1}
+  cat ${1}
 }
 
 ##################################################
-# setJsonString
+# setJsonStringWin
 # 修改json 字符串型字段
 # $1 文件
 # $2 键
 # $3 值
 ##################################################
-setJsonString() {
-  echo "setJsonString($*)"
-  sed -i '' 's/\("'"${2}"'": "\).*/\1'"${3}"'",/g' ${1}
+setJsonStringWin() {
+  echo "setJsonStringWin($*)"
+  sed -i 's/\("'"${2}"'": "\).*/\1'"${3}"'",/g' ${1}
 }
 
 ##################################################
-# uploadPackage
+# uploadPackageWin
 # 将代码打包上传
 # $1 开发者工具路径
 # $2 当前路径
@@ -33,19 +45,73 @@ setJsonString() {
 # $5 版本号
 # $6 版本备注
 ##################################################
-uploadPackage() {
+uploadPackageWin() {
+  echo "uploadPackageWin($*)"
   # 修改小程序标识
-  setJsonBoolean ./src/ext.json extEnable ${3}
-  cat ./src/ext.json
+  setJsonBooleanWin ./src/ext.json extEnable ${3}
   sleep 1
   # 代码编译
   npm run build
   sleep 1
   # 修改开发者工具appid
-  setJsonString ./dist/project.config.json appid ${4}
+  setJsonStringWin ./dist/project.config.json appid ${4}
+  sleep 8
+  # 修正为windows可用的路径
+  WIN_PROJECT_PATH=${2:1:1}:${2:2}
+  echo ${WIN_PROJECT_PATH}
+  # 上传代码
+  ${1} -u ${5}@${WIN_PROJECT_PATH}/dist --upload-desc ${6} --upload-info-output ${WIN_PROJECT_PATH}/uploadinfo.json
+  sleep 1
+  echo "===== 上传完毕 ====="
+}
+
+##################################################
+# setJsonBooleanMac
+# 修改json 布尔型字段
+# $1 文件
+# $2 键
+# $3 值
+##################################################
+setJsonBooleanMac() {
+  echo "setJsonBooleanMac($*)"
+  sed -i '' 's/\("'"${2}"'": \).*/\1'"${3}"',/g' ${1}
+  cat ${1}
+}
+
+##################################################
+# setJsonStringMac
+# 修改json 字符串型字段
+# $1 文件
+# $2 键
+# $3 值
+##################################################
+setJsonStringMac() {
+  echo "setJsonStringMac($*)"
+  sed -i '' 's/\("'"${2}"'": "\).*/\1'"${3}"'",/g' ${1}
+}
+
+##################################################
+# uploadPackageMac
+# 将代码打包上传
+# $1 开发者工具路径
+# $2 当前路径
+# $3 小程序标识（是否专属版）
+# $4 AppId
+# $5 版本号
+# $6 版本备注
+##################################################
+uploadPackageMac() {
+  # 修改小程序标识
+  setJsonBooleanMac ./src/ext.json extEnable ${3}
+  sleep 1
+  # 代码编译
+  npm run build
+  sleep 1
+  # 修改开发者工具appid
+  setJsonStringMac ./dist/project.config.json appid ${4}
   sleep 8
   # 上传代码
-  ${1} -u ${5}@${2}/dist --upload-desc ${6} --upload-info-output ${SHELL_FOLDER}/uploadinfo.json
+  ${1} -u ${5}@${2}/dist --upload-desc ${6} --upload-info-output ${2}/uploadinfo.json
   sleep 1
   echo "===== 上传完毕 ====="
 }
@@ -67,15 +133,17 @@ uploadPackage() {
 ##################################################
 
 echo "=====千米小程序本地打码脚本====="
-#版本号规则
-VERSION_RULE="^[Vv].{1,18}$"
-# 当前路径：${SHELL_FOLDER}
+# 版本号规则
+VERSION_RULE="^[Vv].{1,19}$"
+# 路径
 SHELL_FOLDER=$(cd "$(dirname "$0")";pwd)
+echo "当前路径：${SHELL_FOLDER}"
 # 微信开发者工具路径
 DEVTOOL_PATH_WIN="${WECHAT_DEVTOOL_PATH_CLI}"
 DEVTOOL_PATH_MAC="/Applications/wechatwebdevtools.app/Contents/MacOS/cli"
 DEVTOOL_PATH=""
-# Appid
+SYSTEM_SIGN=""
+# Appid wx821aadcd431646f9
 APPID_SERVICE=wx821aadcd431646f9
 APPID_PLATFORM=wx1cac8cd76fcd3044
 # 版本号
@@ -84,27 +152,47 @@ VERSION_PLATFORM="${1}"
 # 打包信息
 VERSION_INFO="info:${2}"
 
-# 获取开发者工具路径
+# 获取操作系统/开发者工具路径
 if [[ "${DEVTOOL_PATH_WIN}" != "" && -e ${DEVTOOL_PATH_WIN} ]]; then
   DEVTOOL_PATH=${DEVTOOL_PATH_WIN}
+  SYSTEM_SIGN="WIN"
 elif [[ "${DEVTOOL_PATH_MAC}" != "" && -e ${DEVTOOL_PATH_MAC} ]]; then
   DEVTOOL_PATH=${DEVTOOL_PATH_MAC}
+  SYSTEM_SIGN="MAC"
 fi
-if [[ "${DEVTOOL_PATH}" != "" ]]; then
-  echo "微信开发者工具路径：${DEVTOOL_PATH}"
+if [[ "${SYSTEM_SIGN}" != "" ]]; then
+  echo "当前系统为：${SYSTEM_SIGN}，开发者工具路径：${DEVTOOL_PATH}"
 else
   echo "微信开发者工具未找到，可能路径错误。"
   exit
 fi
 
-if [[ ${1} =~ ${VERSION_RULE} ]]; then
-  # 打包专属版
-  uploadPackage ${DEVTOOL_PATH} ${SHELL_FOLDER} true ${APPID_SERVICE} ${VERSION_SERVICE} ${VERSION_INFO}
-  # 打包平台版
-  uploadPackage ${DEVTOOL_PATH} ${SHELL_FOLDER} false ${APPID_PLATFORM} ${VERSION_PLATFORM} ${VERSION_INFO}
-
-  echo -e "专属版本号：${VERSION_SERVICE}\n平台版本号：${VERSION_PLATFORM}"
+# 打包
+if [[ "${SYSTEM_SIGN}" == "WIN" ]]; then
+  # 打包WIN
+  if [[ ${1} =~ ${VERSION_RULE} ]]; then
+    # 打包专属版
+    uploadPackageWin ${DEVTOOL_PATH} ${SHELL_FOLDER} true ${APPID_SERVICE} ${VERSION_SERVICE} ${VERSION_INFO}
+	# 打包平台版
+    uploadPackageWin ${DEVTOOL_PATH} ${SHELL_FOLDER} false ${APPID_PLATFORM} ${VERSION_PLATFORM} ${VERSION_INFO}
+    echo -e "专属版本号：${VERSION_SERVICE}\n平台版本号：${VERSION_PLATFORM}"
+  else
+    echo "版本号：${1} 非正常版本号，无法上传打码。"
+    exit
+  fi
+elif [[ "${SYSTEM_SIGN}" == "MAC" ]]; then
+  # 打包MAC
+  if [[ ${1} =~ ${VERSION_RULE} ]]; then
+    # 打包专属版
+    uploadPackageMac ${DEVTOOL_PATH} ${SHELL_FOLDER} true ${APPID_SERVICE} ${VERSION_SERVICE} ${VERSION_INFO}
+    # 打包平台版
+    uploadPackageMac ${DEVTOOL_PATH} ${SHELL_FOLDER} false ${APPID_PLATFORM} ${VERSION_PLATFORM} ${VERSION_INFO}
+    echo -e "专属版本号：${VERSION_SERVICE}\n平台版本号：${VERSION_PLATFORM}"
+  else
+    echo "版本号：${1} 非正常版本号，无法上传打码。"
+    exit
+  fi
 else
-  echo "版本号：${1} 非正常版本号，无法上传打码。"
+  echo "异常错误。"
   exit
 fi
